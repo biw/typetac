ae08b3ffe8d9bb5426b33a2f54a7e50ce396bb93
Ben Williams <ben.williams@colorado.edu>
2021-05-29 15:03:35 -0700

    more simple build

--- esbuild.js
+++ esbuild.js
@@ -1,18 +1,14 @@
 const { build } = require('esbuild')
 
-const formats = ['cjs', 'iife']
-
-formats.forEach((format) => {
-  build({
-    bundle: true,
-    target: 'es6',
-    platform: 'node',
-    color: true,
-    sourcemap: 'external',
-    logLevel: 'info',
-    format,
-    entryPoints: ['./src/index.ts'],
-    outfile: format === 'cjs' ? './dist/typetac.js' : './dist/typetac.min.js',
-    minify: true,
-  })
+build({
+  bundle: true,
+  target: 'es6',
+  platform: 'node',
+  color: true,
+  sourcemap: 'external',
+  logLevel: 'info',
+  format: 'cjs',
+  entryPoints: ['./src/index.ts'],
+  outfile: './dist/typetac.js',
+  minify: true,
 })
