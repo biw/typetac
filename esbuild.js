8d11a51410bff57f94800091f1ba3becd2b60123
Ben Williams <ben.williams@colorado.edu>
2021-05-29 15:22:18 -0700

    moreved unneed vars

--- esbuild.js
+++ esbuild.js
@@ -2,12 +2,9 @@ const { build } = require('esbuild')
 
 build({
   bundle: true,
-  target: 'es6',
+  target: ['es6', 'node6'],
   platform: 'node',
-  color: true,
   sourcemap: 'external',
-  logLevel: 'info',
-  format: 'cjs',
   entryPoints: ['./src/index.ts'],
   outfile: './dist/typetac.js',
   minify: true,
